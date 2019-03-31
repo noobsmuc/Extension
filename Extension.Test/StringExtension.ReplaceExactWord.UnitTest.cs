61ac48b2e3e951008745e553149027117ff44ca1
Axel Katzur <axel@katzur.de>
2019-03-31 17:52:09 +0200

    add xml test for ReplacExactWord

--- Extension.Test/StringExtension.ReplaceExactWord.UnitTest.cs
+++ Extension.Test/StringExtension.ReplaceExactWord.UnitTest.cs
@@ -19,5 +19,15 @@ namespace NoobsMuc.Extension
         {
             InputString.ReplaceExactWord(">=", "!=").Should().Be("Add != Additional < String");
         }
+
+        [Test]
+        public void ReplaceExactWord_StringToReplaceWithXml_ReturnStringWithTheReplacedWord()
+        {
+            string act = "<ReplaceMe>ReplaceMeNotHere</ReplaceMe>";
+
+            string result = act.ReplaceExactWord("ReplaceMe", "OtherValue");
+                
+            result.Should().Be("<OtherValue>ReplaceMeNotHere</OtherValue>");
+        }
     }
 }
